b34930a12a954d1fae50718ea6ed6379adf02d56
administrator <administrator@6370.local>
2021-03-27 13:40:29 -0700

    add controller adicionarPessoa

--- Festa/Festa/Controller/AdiconarPessoaViewController.swift
+++ Festa/Festa/Controller/AdiconarPessoaViewController.swift
@@ -6,7 +6,25 @@ class AdicionarPessoaViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBOutlet weak var txtNome: UITextField?
+    @IBOutlet weak var txtIdade: UITextField?
+    
+    @IBAction func cadastrarPessoa() {
+        print("Cadastrando..")
+        
+        guard let nome = txtNome?.text else{
+            print("Informe o nome");
+            return
+        }
+        guard let idade = txtIdade?.text else {
+            print("Informe a idade");
+            return
+        }
+        
+        print("Nome: \(nome) Idade: \(idade) ")
+    }
+    
 }
 
