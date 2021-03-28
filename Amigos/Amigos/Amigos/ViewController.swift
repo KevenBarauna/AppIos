4cff9f2529059ba8d3476097029bd9d599ca8639
administrator <administrator@6370.local>
2021-03-28 14:10:41 -0700

    ui

---
+++ Amigos/Amigos/Amigos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amigos
+//
+//  Created by administrator on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
